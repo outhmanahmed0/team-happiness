4a220037c8ad09acb07483029b23e8e1ee5ee558
Hussein <almadhachihussein@gmail.com>
2025-12-05 12:37:00 +0300

    initial project scafolding

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # team-happiness
 Image to text conversion platform
+
+## NOTE
+there was an npm outage so node_modules are uploaded
---
+++ frontend/svelte.config.js
@@ -0,0 +1,15 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+
+/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    // ... other config
+    alias: {
+      "@/*": "./src/lib/*",
+    },
+  }
+}
+
